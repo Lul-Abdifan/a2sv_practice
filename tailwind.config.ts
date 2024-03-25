b6b226d5c2a654d0feadac8e5d21a83016de5d09
Lul-Abdifan <tasanbannu@gmail.com>
2024-03-25 09:17:17 +0300

    Update detail styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,15 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['var(--font-poppins)'],
+        epilogue: ['var(--font-epilogue)'],
+      },
+      
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      // fontFamily:{
-      //   Epilogue:['var(--font-Epilogue)']
-        
-      // }
+
     },
   },
   plugins: [],
